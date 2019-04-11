d444015f3c45d8f682dad8c6f4fc471c1e7b0224
anton <anton@asus555.com>
2019-04-11 21:36:03 +0300

    deleted unnecessary comments

--- gumbo-wrapper/Document.cpp
+++ gumbo-wrapper/Document.cpp
@@ -78,14 +78,6 @@ std::string str_join(std::vector<std::string>& vec) {
     std::string str;
 
     for (auto x : vec) {
-		/*
-        std::string var;
-
-        for (auto y : x)
-            var += y;
-
-        str += var;
-		*/
 		str += x;
     }
 
